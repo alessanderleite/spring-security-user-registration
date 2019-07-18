8529a903fc3b3c7a9872387265fd094291ed1ab0
alessanderleite <alessander.logistica@gmail.com>
2019-07-17 23:31:57 -0300

    code the FieldMatchValidator.java class

--- src/main/java/br/com/alessanderleite/springsecurity/constraint/FieldMatchValidator.java
+++ src/main/java/br/com/alessanderleite/springsecurity/constraint/FieldMatchValidator.java
@@ -3,19 +3,27 @@ package br.com.alessanderleite.springsecurity.constraint;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object>{
 	private String firstFieldName;
 	private String secondFieldName;
 	
 	@Override
 	public void initialize(final FieldMatch constraintAnnotation) {
-		firstFieldName = constraintAnnotation
+		firstFieldName = constraintAnnotation.first();
+		secondFieldName = constraintAnnotation.second();
 	}
 	
 	@Override
-	public boolean isValid(Object value, ConstraintValidatorContext context) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+		try {
+			final Object firstObject = BeanUtils.getProperty(value, firstFieldName);
+			final Object secondObject = BeanUtils.getProperty(value, secondFieldName);
+			return firstObject == null && secondObject == null || firstObject != null && firstObject.equals(secondObject);
+		} catch (final Exception ignore) {}
+		
+		return true;
 	}
 
 }
